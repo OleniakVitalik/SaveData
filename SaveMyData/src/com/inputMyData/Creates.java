f80f941ce961bb19dde0ba51834e367d6f54a60b
OleniakVitaliy <voleniak@omilia.com>
2016-07-14 18:04:20 +0300

    New

--- SaveMyData/src/com/inputMyData/Creates.java
+++ SaveMyData/src/com/inputMyData/Creates.java
@@ -32,10 +32,8 @@ public class Creates extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request, response);
-
 		String url = request.getParameter("url");
 		String password = request.getParameter("password");
 		String login = request.getParameter("login");
@@ -52,6 +50,16 @@ public class Creates extends HttpServlet {
 			e.printStackTrace();
 		}
 		request.getRequestDispatcher("/inputData.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+
 	}
 
 	private void insertIntoDB(String url, String password, String login, String used, Connection connection)
